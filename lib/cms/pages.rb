9be61fa1130e8a1f07a914f38e082058027c93b5
VoroninNick <p.korenev@voroninstudio.eu>
2016-03-25 12:40:34 +0200

    fix

--- lib/cms/pages.rb
+++ lib/cms/pages.rb
@@ -3,11 +3,13 @@ module Pages
     def method_missing(name, *args, &block)
       normalized_name = name.to_s.camelize
 
-      if Pages.constants.map(&:to_s).include?(normalized_name)
-        return Pages.const_get(normalized_name).first_or_initialize
-      end
+      #if Pages.constants.map(&:to_s).include?(normalized_name)
 
-      super(name, *args, &block)
+      #end
+
+      return Pages.const_get(normalized_name).first_or_initialize rescue super(name, *args, &block)
+
+
     end
 
     def create_pages
