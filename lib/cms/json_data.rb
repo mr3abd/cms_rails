6ab7318d07c43ff12c666d9782aa855e79f9f520
VoroninNick <p.korenev@voroninstudio.eu>
2016-03-25 19:01:18 +0200

    add integer type for json_data field

--- lib/cms/json_data.rb
+++ lib/cms/json_data.rb
@@ -1,15 +1,15 @@
 module JsonData
   module ActiveRecordExtensions
     module ClassMethods
-      def field name, force = false
+      def field name, type = :string, force = false
         if force || !self.class_variable_defined?(:@@_fields)
-          self.class_variable_set :@@_fields, []
+          self.class_variable_set :@@_fields, {}
           after_initialize :assign_attributes_from_json_field
           before_save :serialize_attributes_to_json_field
         end
         fields = self.class_variable_get(:@@_fields)
-        if !fields.include?(name)
-          fields << name
+        if !fields.keys.map(&:to_s).include?(name)
+          fields[name.to_sym] = { type: type }
           self.class_variable_set :@@_fields, fields
           attr_accessor name
           attr_accessible name
@@ -18,7 +18,7 @@ module JsonData
 
       def fields *names, **opts
         names.each_with_index do |name, index|
-          field name, index > 0
+          field name, :string, index > 0
         end
       end
     end
@@ -42,10 +42,16 @@ module JsonData
       end
 
       def serialize_attributes_to_json_field json_field = :json_data
-        field_names = self.class.class_variable_get(:@@_fields)
+        fields = self.class.class_variable_get(:@@_fields)
         data = {}
-        field_names.each do |field_name|
-          data[field_name.to_sym] = self.instance_variable_get("@#{field_name}")
+        fields.each do |field_name, field_definition|
+          v = self.instance_variable_get("@#{field_name}")
+          converted_value = v
+          if field_definition[:type].to_s == 'integer'
+            converted_value = v.to_i
+          end
+
+          data[field_name.to_sym] = converted_value
         end
         send("#{json_field}=", data.to_json)
 
