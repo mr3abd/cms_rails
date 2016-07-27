3a6f7c1fc1cbda3fd619669f34fd839b780350db
pasha <p.korenev@voroninstudio.eu>
2016-07-27 16:40:57 +0300

    app

---
+++ lib/cms/migration_extensions.rb
@@ -0,0 +1,19 @@
+module ActiveRecord
+  module ConnectionAdapters # :nodoc:
+    module SchemaStatements
+      def create_form_table(table_name, &block)
+        create_table table_name do |t|
+          t.string :referer
+          t.integer :session_id
+          t.string :locale
+
+          if block_given?
+            block.call
+          end
+
+          t.timestamps null: false
+        end
+      end
+    end
+  end
+end
