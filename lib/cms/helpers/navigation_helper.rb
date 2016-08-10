4b6e5a5cf772229a4347bef104f859d864b983a7
pasha <p.korenev@voroninstudio.eu>
2016-08-10 16:40:24 +0300

    fix

--- lib/cms/helpers/navigation_helper.rb
+++ lib/cms/helpers/navigation_helper.rb
@@ -61,7 +61,7 @@ module Cms
 
 
           if check_for_active
-            active = controller_name == key || (action_name == key && controller_name == "pages") || params[:route_name].to_s == key || @page_instance.try(:url)
+            active = controller_name == key || (action_name == key && controller_name == "pages") || params[:route_name].to_s == key || @page_instance.try(:url) == entry[:url]
             entry[:active] = active
           end
 
