bb02862e697abe2fe57fd528eb2e030292d596df
pasha <p.korenev@voroninstudio.eu>
2016-08-15 19:41:55 +0300

    weather

--- app/models/cms/weather_data.rb
+++ app/models/cms/weather_data.rb
@@ -1,11 +1,13 @@
+require 'httparty'
+
 class Cms::WeatherData < ActiveRecord::Base
   self.table_name = :weather_data
 
   field :result
 
-  def self.actual(provider = :nbu)
+  def self.actual(provider = :openweathermap)
     instance = self.where(provider: provider).last
-    is_actual = instance.nil? || instance.created_at.blank? ? false : DateTime.now - 24.hours < instance.created_at
+    is_actual = instance.nil? || instance.created_at.blank? ? false : DateTime.now - 60.minutes < instance.created_at
     if !is_actual
       instance = self.new
       instance.send("store_#{provider}")
@@ -14,6 +16,13 @@ class Cms::WeatherData < ActiveRecord::Base
   end
 
   def store_openweathermap(api_key, city = "Lviv")
-
+    response = HTTParty.get('http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json', timeout: 10) rescue nil
+    if response
+      parsed = JSON.parse(response)
+      self.result = parsed
+      self.provider = "openweathermap"
+    else
+      []
+    end
   end
 end
