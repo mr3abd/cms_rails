931079c4fff7de325f71364de375d0c434d1d8ba
pasha <p.korenev@voroninstudio.eu>
2016-08-15 18:54:46 +0300

    fix

--- app/models/cms/exchange_rate.rb
+++ app/models/cms/exchange_rate.rb
@@ -42,7 +42,7 @@ class Cms::ExchangeRate < ActiveRecord::Base
   end
 
   def convert(amount = 1, input_currency = :usd, output_currency = :uah, direction = :sale)
-    if self.provider == :private_bank
+    if self.provider.to_sym == :private_bank
       item = result.select{|item| item['ccy'] == input_currency.upcase && item['base_ccy'] == output_currency.upcase  }.first
       if !item
         return nil
