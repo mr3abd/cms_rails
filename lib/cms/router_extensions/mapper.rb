dd5c8eb706773b230125fef13806340bb23c08c8
pasha <p.korenev@voroninstudio.eu>
2016-08-18 14:06:39 +0300

    router aliases: domain->domains, host->hosts

--- lib/cms/router_extensions/mapper.rb
+++ lib/cms/router_extensions/mapper.rb
@@ -6,9 +6,17 @@ module ActionDispatch
         constraints(DomainConstraint.new(addresses), &block)
       end
 
+      def domain(*args, &block)
+        domains(*args, &block)
+      end
+
       def hosts(*args, &block)
         addresses = args.flatten
         constraints(HostConstraint.new(addresses), &block)
+      end
+
+      def host(*args, &block)
+        hosts(*args, &block)
       end
     end
   end
