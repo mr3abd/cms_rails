fe833ec1b7619d9020c29542a92a0cf4c0f2ef02
pasha <p.korenev@voroninstudio.eu>
2016-08-25 16:57:20 +0300

    fix

--- lib/cms.rb
+++ lib/cms.rb
@@ -143,7 +143,7 @@ module Cms
           next_key_args = args
           next_keys = args.select{|a| break false if a.is_a?(Hash); next true }
           str = t(*next_key_args, options) if str.blank? && next_keys.any?
-          str = key.split(".").last.to_s.humanize
+          str = key.split(".").last.to_s.humanize if str.blank?
         end
       end
 
