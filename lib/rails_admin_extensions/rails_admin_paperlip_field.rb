3c6fd5737ac43fe586b815c329079136b787cac0
pasha <p.korenev@voroninstudio.eu>
2016-08-31 14:20:44 +0300

    paperclip in rails_admin: show image size

---
+++ lib/rails_admin_extensions/rails_admin_paperlip_field.rb
@@ -0,0 +1,25 @@
+require 'rails_admin/config/fields/types/paperclip'
+
+module RailsAdmin
+  module Config
+    module Fields
+      module Types
+        # Field type that supports Paperclip file uploads
+        class Paperclip < RailsAdmin::Config::Fields::Types::FileUpload
+          def generic_field_help
+            str = super
+            #size = bindings[:object]
+            styles = abstract_model.model.attachment_definitions[name.to_sym][:styles]
+            if styles.try(:any?)
+              size = styles.map{|k, v| w, h = v.split("x"); w = w.to_i; fit_size = h.end_with?("#"); h = h.to_i; square = w * h; [k, v, square] }.max_by{|a| a[2] }[1]
+            else
+              size = ""
+            end
+
+            "#{str} #{size}"
+          end
+        end
+      end
+    end
+  end
+end
