eb2cf6cd81410ad19259f863a73b46db81f65608
pasha <p.korenev@voroninstudio.eu>
2016-09-02 13:21:57 +0300

    translation from Pages, Cms::Text, yml

--- lib/cms.rb
+++ lib/cms.rb
@@ -162,7 +162,28 @@ module Cms
       str.to_s.html_safe
     end
 
+    def find_translation_in_pages(*args)
+      @t_page_classes ||= Hash[Pages.all_instances.select{|c| c.try(:page_info).present? }.map{|p| c = p.class; pic = c.page_info_class; [c.name.split("::").last.underscore, {attribute_names: pic.attribute_names, page_info: p.page_info, page: p}] }] rescue nil
+      res = nil
+      if @t_page_classes.try(:any?)
+        args.take_while{|a| a.is_a?(String) || a.is_a?(Symbol) }.each do |str|
+          key_parts = str.split(".")
+          if key_parts.length == 2 && (page_hash = @t_page_classes[key_parts.first]) && page_hash[:attribute_names].include?(key_parts[1])
+            res = page_hash[:page].try(key_parts[1])
+            res = page_hash[:page_info].try(key_parts[1]) if res.blank?
+          end
+
+          return res if res.present?
+        end
+      end
+    end
+
     def t(*args)
+      res = find_translation_in_pages(*args)
+      if res.present?
+        return res
+      end
+
       keys = args.take_while{|a| a.is_a?(Symbol) || a.is_a?(String) || a.is_a?(Array) }.flatten
       options = args.last.is_a?(Hash) ? args.last : {}
       text_model = Text rescue Cms::Text rescue nil
--- lib/cms/helpers/translation_helper.rb
+++ lib/cms/helpers/translation_helper.rb
@@ -1,20 +1,16 @@
 module Cms
   module Helpers
     module TranslationHelper
-      def t(*args)
-        @t_page_classes ||= Hash[Pages.all_instances.select{|c| c.try(:page_info).present? }.map{|p| c = p.class; pic = c.page_info_class; [c.name.split("::").last.underscore, {attribute_names: pic.attribute_names, page_info: p.page_info, page: p}] }] rescue nil
-        res = nil
-        if @t_page_classes.try(:any?)
-          args.take_while{|a| a.is_a?(String) || a.is_a?(Symbol) }.each do |str|
-            key_parts = str.split(".")
-            if key_parts.length == 2 && (page_hash = @t_page_classes[key_parts.first]) && page_hash[:attribute_names].include?(key_parts[1])
-              res = page_hash[:page].try(key_parts[1])
-              res = page_hash[:page_info].try(key_parts[1]) if res.blank?
-            end
-
-            return res if res.present?
-          end
+      def self.included(base)
+        methods = self.instance_methods
+        methods.delete(:included)
+        if base.respond_to?(:helper_method)
+          base.helper_method methods
         end
+
+      end
+
+      def t(*args)
         Cms.t(*args)
       end
     end
