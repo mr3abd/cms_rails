1b491587566f64039820bcb17f853a3ea5a6333b
pasha <p.korenev@voroninstudio.eu>
2016-09-21 12:36:20 +0300

    both Table and TableDefinition

--- lib/cms/activerecord_migration_columns.rb
+++ lib/cms/activerecord_migration_columns.rb
@@ -1,13 +1,19 @@
-class ActiveRecord::ConnectionAdapters::TableDefinition
-
-  def linkable(*args)
-    options = args.extract_options!
-    name = args[0]
-    name = :linkable if name.blank?
-    #options.reverse_merge!({ precision: 8, scale: 2 })
-    #column_names = args
-    column("#{name}_id", :integer, options)
-    column("#{name}_type", :string, options)
+module ActiveRecordExtensions
+  module Linkable
+    module TableDefinition
+      def linkable(*args)
+        options = args.extract_options!
+        name = args[0]
+        name = :linkable if name.blank?
+        #options.reverse_merge!({ precision: 8, scale: 2 })
+        #column_names = args
+        column("#{name}_id", :integer, options)
+        column("#{name}_type", :string, options)
+      end
+    end
   end
-
 end
+
+ActiveRecord::ConnectionAdapters::TableDefinition.send :include, ActiveRecordExtensions::Linkable::TableDefinition
+ActiveRecord::ConnectionAdapters::Table.send :include, ActiveRecordExtensions::Linkable::TableDefinition
+
