78255d4732a59766121de337d8ac917a88104f19
pasha <p.korenev@voroninstudio.eu>
2016-10-20 19:31:10 +0300

    shortcuts

--- lib/cms/shortcuts.rb
+++ lib/cms/shortcuts.rb
@@ -1,7 +1,12 @@
 module Cms
   module Shortcuts
-    def tables
-      ActiveRecord::Base.connection.tables.sort
+    def tables(mask = nil)
+      arr = ActiveRecord::Base.connection.tables.sort
+      if mask.is_a?(Regexp)
+        arr.select{|item| item.to_s.scan(mask).any? }
+      else
+        arr
+      end
     end
   end
 end
