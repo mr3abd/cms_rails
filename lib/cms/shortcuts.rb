fbd5b35e5b7a92c4fb251f2fe6ae5f0f464c2b1c
pasha <p.korenev@voroninstudio.eu>
2016-10-20 19:34:09 +0300

    lib

--- lib/cms/shortcuts.rb
+++ lib/cms/shortcuts.rb
@@ -4,6 +4,8 @@ module Cms
       arr = ActiveRecord::Base.connection.tables.sort
       if mask.is_a?(Regexp)
         arr.select{|item| item.to_s.scan(mask).any? }
+      elsif mask.is_a?(String) || mask.is_a?(Symbol)
+        arr.select{|item| item.to_s.include?(mask.to_s) }
       else
         arr
       end
