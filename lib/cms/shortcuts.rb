edae8fa2590ce9933206518dc5176ed03c7cbcd7
pasha <p.korenev@voroninstudio.eu>
2016-10-20 19:42:43 +0300

    shortcuts

--- lib/cms/shortcuts.rb
+++ lib/cms/shortcuts.rb
@@ -2,6 +2,19 @@ module Cms
   module Shortcuts
     def tables(mask = nil)
       arr = ActiveRecord::Base.connection.tables.sort
+      filter(arr, mask)
+    end
+
+    def columns(table_name)
+      ActiveRecord::Base.connection.columns(table_name)
+    end
+
+    def column_names(table_name, mask = nil)
+      arr = ActiveRecord::Base.connection.columns(table_name).map(&:name)
+      filter(arr, mask).sort
+    end
+
+    def filter(array, mask = nil)
       if mask.is_a?(Regexp)
         arr.select{|item| item.to_s.scan(mask).any? }
       elsif mask.is_a?(String) || mask.is_a?(Symbol)
