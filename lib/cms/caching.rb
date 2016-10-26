f49d1d1d0db0179c41292444f681bbb7e6ef7ce9
pasha <p.korenev@voroninstudio.eu>
2016-10-26 21:34:12 +0300

    fix

--- lib/cms/caching.rb
+++ lib/cms/caching.rb
@@ -124,7 +124,7 @@ module Cms
             public_path = Rails.root.join("public").to_s
             public_path = public_path[0, public_path.length - 1] if public_path.end_with?("/")
             relative_path = p
-            relative_path = "/#{p}" if !start_with?("/")
+            relative_path = "/#{p}" if !relative_path.start_with?("/")
             path = "#{public_path}#{relative_path}"
             gzipped_path = "#{path}.gz"
             #puts "path: #{path}"
