5adfb616e1c497805078eb1bc45c52cce1ceae2b
pasha <p.korenev@voroninstudio.eu>
2016-10-26 21:32:30 +0300

    fix

--- lib/cms/caching.rb
+++ lib/cms/caching.rb
@@ -120,15 +120,15 @@ module Cms
 
         expired_pages = expired_pages.uniq
         if filter_existing
-          expired_pages = expired_fragments.select{|p|
+          expired_pages = expired_pages.select{|p|
             public_path = Rails.root.join("public").to_s
             public_path = public_path[0, public_path.length - 1] if public_path.end_with?("/")
             relative_path = p
             relative_path = "/#{p}" if !start_with?("/")
             path = "#{public_path}#{relative_path}"
             gzipped_path = "#{path}.gz"
-            puts "path: #{path}"
-            puts "gzipped_path: #{gzipped_path}"
+            #puts "path: #{path}"
+            #puts "gzipped_path: #{gzipped_path}"
             File.exists?(path) || File.exists?(gzipped_path)
           }
         end
