c60bf5400d1edddc31e859297cf5c8b9e1210085
pasha <p.korenev@voroninstudio.eu>
2016-11-05 15:45:01 +0200

    lib

---
+++ lib/cms/standard_library_extensions/hash.rb
@@ -0,0 +1,11 @@
+class Hash
+  def in_groups_of(count, allow_nil = nil)
+    h = self
+    h.map{|k, v| {:"#{k}" => v} }.flatten.in_groups_of(count , allow_nil).map{|group| Hash[group.map{|e| [e.keys.first, e.first.second] }] }
+  end
+
+  def in_groups(count, allow_nil)
+    h = self
+    in_groups_of((h.keys.count.to_f / count).ceil, allow_nil)
+  end
+end
