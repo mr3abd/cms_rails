b07d4d46791038f53b06fe67044d04c1cdb743b1
pasha <p.korenev@voroninstudio.eu>
2016-11-25 13:14:06 +0200

    json_field

--- lib/cms/json_data.rb
+++ lib/cms/json_data.rb
@@ -18,11 +18,16 @@ module JsonData
           field_hash = { type: type, group: :default }.merge(options)
           fields[name.to_sym] = field_hash
           self.class_variable_set :@@_fields, fields
-          attr_accessor name
+          #attr_accessor name
           attr_accessible name
-          define_method "#{name}=" do
+
+          define_method "#{name}=" do |val|
+            puts "hello"
+            instance_variable_set(:"@#{name}", val)
             serialize_attributes_to_json_field
+
           end
+
         end
       end
 
