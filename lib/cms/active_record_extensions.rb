3cbd17dbb69051724f8594aa6664918c7a7aae44
pkorenev <p.korenev@voroninstudio.eu>
2017-03-20 10:42:19 +0200

    pages

--- lib/cms/active_record_extensions.rb
+++ lib/cms/active_record_extensions.rb
@@ -463,8 +463,8 @@ module Cms
 
     if Cms::Page.include_translations?
       Cms::Page.initialize_globalize
-      puts "translated: #{Cms::Page.translated_attribute_names}"
-      Cms::Page.create_translation_table!(url: :string, content: :text, name: :string)
+      #puts "translated: #{Cms::Page.translated_attribute_names}"
+      Cms::Page.create_translation_table(:url, :content, :name )
     end
   end
 
