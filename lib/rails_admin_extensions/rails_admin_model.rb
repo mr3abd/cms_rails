321529b8209bed402f6a880dd9ff1391aa217b24
pkorenev <p.korenev@voroninstudio.eu>
2017-03-27 20:46:04 +0300

    rails_admin navigation_label_key

---
+++ lib/rails_admin_extensions/rails_admin_model.rb
@@ -0,0 +1,33 @@
+RailsAdmin::Config::Model.class_eval do
+  def navigation_label_key(k, weight = 0)
+    navigation_label do
+      I18n.t("admin.navigation_labels.#{k}")
+    end
+    if weight
+      model_weight(weight, k)
+    end
+  end
+
+  def model_weight(rel_weight, navigation_label)
+    weights = {
+        feedbacks: 100,
+        home: 200,
+        about_us: 300,
+        projects: 400,
+        partnership: 500,
+        brands: 600,
+        services: 700,
+        media: 800,
+        contacts: 900,
+        tags: 1000,
+        users: 1100,
+        settings: 1200,
+        pages: 1300,
+        assets: 1400
+    }
+    navigation_label_weight = weights[navigation_label.to_sym]
+    computed_weight = navigation_label_weight + rel_weight
+    weight computed_weight
+  end
+
+end
