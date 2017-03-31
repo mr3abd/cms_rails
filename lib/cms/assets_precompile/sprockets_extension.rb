6790b020b66e459dd28abae5d4f14377f9f52073
pkorenev <p.korenev@voroninstudio.eu>
2017-03-31 15:02:01 +0300

    app

--- lib/cms/assets_precompile/sprockets_extension.rb
+++ lib/cms/assets_precompile/sprockets_extension.rb
@@ -71,6 +71,7 @@ module Cms
             logger.set("total_files", normalized_args.flatten.count)
 
             find(*normalized_args) do |asset|
+              next if ENV["debug_precompile"]
               next if !Cms::AssetsPrecompile::SprocketsExtension.precompile_file?(asset.logical_path)
               current_file_number += 1
               files[asset.digest_path] = {
