0c5448d50eb08516de14efb1d2eb74fb4e547bde
pasha <p.korenev@voroninstudio.eu>
2017-04-13 15:45:21 +0300

    app

--- lib/cms/human_attribute_name.rb
+++ lib/cms/human_attribute_name.rb
@@ -4,8 +4,18 @@ module Cms
       attr = attr.to_s
       model_key = self.name.underscore
       versions = ["activerecord.attributes.#{model_key}.#{attr}", "activerecord.attributes.#{attr}"]
-      str = Cms.t(versions)
-      if str.is_a?(Hash) || str.is_a?(Array)
+      str = nil
+      versions.each do |v|
+        str = I18n.t(v, raise: true) rescue nil
+        if str.is_a?(Hash) || str.is_a?(Array)
+          str = nil
+        end
+        break if str.present?
+      end
+
+
+
+      if str.blank?
         model_key.humanize
       else
         str
