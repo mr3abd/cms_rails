ec2f399e0f57cae489b164f55e0c811aa92786e3
pasha <p.korenev@voroninstudio.eu>
2017-05-26 15:15:02 +0300

    app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cms::Engine.routes.draw do
   if Rails.env.production? && ENV["GOOGLE_WEB_MASTER_ID"].present?
-    get "google#{ENV["GOOGLE_WEB_MASTER_ID"]}.html", format: false, to: "cms/google#web_master", as: :google_web_master_confirmation
+    get "google#{ENV["GOOGLE_WEB_MASTER_ID"]}.html", format: false, to: "google#web_master", as: :google_web_master_confirmation
   end
 
   match '/file_editor/(*path)', to: 'file_editor#index', via: [:get, :post], format: false, as: :file_editor
