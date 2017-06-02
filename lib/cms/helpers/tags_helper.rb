a7c6cc13349a7c7e41a3f04715132af51f398cca
pasha <p.korenev@voroninstudio.eu>
2017-06-02 12:43:43 +0300

    app

--- lib/cms/helpers/tags_helper.rb
+++ lib/cms/helpers/tags_helper.rb
@@ -1,6 +1,13 @@
 module Cms
   module Helpers
     module TagsHelper
+      def self.included(base)
+        methods = [:h1_text]
+        if base.respond_to?(:helper_method)
+          base.helper_method methods
+        end
+      end
+
       def h1_text(key_or_instance = nil)
         instance = key_or_instance || @page_instance
         if instance.is_a?(String) || instance.is_a?(Symbol)
