883b149ca8483ea3840119a0d4c2b641da843d87
pasha <p.korenev@voroninstudio.eu>
2017-06-02 12:47:20 +0300

    app

--- lib/cms/helpers/tags_helper.rb
+++ lib/cms/helpers/tags_helper.rb
@@ -11,10 +11,12 @@ module Cms
       def h1_text(key_or_instance = nil)
         instance = key_or_instance || @page_instance
         if instance.is_a?(String) || instance.is_a?(Symbol)
+          key = instance
           instance = Pages.send(instance)
         end
 
-        instance.try(:h1_text)
+        s = instance.try(:h1_text)
+        s.blank? ? instance.try(:page_key) : key.try(:to_s)
       end
     end
   end
