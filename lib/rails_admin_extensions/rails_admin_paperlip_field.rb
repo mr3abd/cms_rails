ee1ef3858e0a45bbd9e6b881dec624054af53685
pasha <p.korenev@voroninstudio.eu>
2017-06-14 20:47:21 +0300

    app

--- lib/rails_admin_extensions/rails_admin_paperlip_field.rb
+++ lib/rails_admin_extensions/rails_admin_paperlip_field.rb
@@ -13,7 +13,19 @@ module RailsAdmin
             return str if attachment_definition.blank?
             styles = attachment_definition[:styles]
             if styles.try(:any?)
-              size = styles.map{|k, v| w, h = v.split("x"); w = w.to_i; fit_size = h.end_with?("#"); h = h.to_i; square = w * h; [k, v, square] }.max_by{|a| a[2] }[1]
+              size = styles.map{|k, v|
+                if v.is_a?(Hash)
+                  next nil
+                else
+                  w, h = v.split("x");
+                end
+
+                w = w.to_i;
+                fit_size = h.end_with?("#");
+                h = h.to_i;
+                square = w * h;
+                [k, v, square]
+              }.select{|a| !a.nil? }.max_by{|a| a[2] }[1]
             else
               size = ""
             end
