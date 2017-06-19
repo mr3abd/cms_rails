ba9aaebf9d56e7706cd16e92b198bc5ecb111455
pasha <p.korenev@voroninstudio.eu>
2017-06-19 16:00:21 +0300

    app

--- lib/cms/text_fields.rb
+++ lib/cms/text_fields.rb
@@ -32,8 +32,10 @@ module Cms
       lines = properties_str.split("\r\n")
       props = Hash[lines.map{|line|
         i = line.index(":")
-        if !keep_empty_values && i.nil? || i < 0
-          next nil
+        if 1 == 1
+          if !keep_empty_values && (i.nil? || i < 0)
+            next nil
+          end
         end
 
         k = line[0, i]
