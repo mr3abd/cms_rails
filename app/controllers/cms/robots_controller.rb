3e678344e01ad4d63ad503dea3190d3975f2d2ec
pkorenev <p.korenev@voroninstudio.eu>
2017-07-29 18:00:20 +0300

    app

--- app/controllers/cms/robots_controller.rb
+++ app/controllers/cms/robots_controller.rb
@@ -3,7 +3,21 @@ module Cms
     caches_page :robots_txt
 
     def robots_txt
-      render "robots.txt"
+      @lines = lines
+      render inline: lines.join("\r\n")
     end
+
+    def lines
+      arr = ["User-agent: *"]
+      if Rails.env.production?
+        arr << "Allow: /"
+      else
+        arr << "Disallow: /"
+      end
+      arr << "Sitemap: #{absolute_url("/sitemap.xml")}"
+      arr
+    end
+
+
   end
 end
