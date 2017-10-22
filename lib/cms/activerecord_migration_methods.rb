355e72d989f8c6d9f2fee1c070209c5f709d6789
pkorenev <p.korenev@voroninstudio.eu>
2017-10-22 12:17:43 +0300

    app

--- lib/cms/activerecord_migration_methods.rb
+++ lib/cms/activerecord_migration_methods.rb
@@ -3,10 +3,14 @@ module ActiveRecordExtensions
     module SchemaStatements
       def create_translation_table(model, *columns)
         if self.reverting?
-          return model.drop_translation_table!
+          model.drop_translation_table!
+          puts "-- #{model.name}.drop_translation_table # #{model.translation_class.table_name}"
+          return
         end
 
         model.create_translation_table(*columns)
+        puts "-- #{model.name}.create_translation_table # #{model.translation_class.table_name}"
+
       end
     end
   end
