f7bd04a23b745bd51582e06714dea1dbdf7064f5
pkorenev <p.korenev@voroninstudio.eu>
2018-01-30 13:04:53 +0200

    app

--- lib/cms/globalize_extension.rb
+++ lib/cms/globalize_extension.rb
@@ -212,7 +212,7 @@ module Cms
     def _create_translation_table
       model = self
       connection.create_table(translations_table_name) do |t|
-        t.references table_name.sub(/^#{table_name_prefix}/, '').singularize, :null => false, :index => false, :type => column_type(model.primary_key).to_sym
+        t.references table_name.sub(/^#{table_name_prefix}/, '').singularize, :null => false, :index => false
         t.string :locale, :null => false
         t.timestamps :null => false
       end
