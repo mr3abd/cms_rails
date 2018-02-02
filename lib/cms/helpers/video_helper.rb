7a4bc2c70fda49aaed0a1c2d776e9056ea553047
pasha <p.korenev@voroninstudio.eu>
2018-02-02 14:29:24 +0200

    video

--- lib/cms/helpers/video_helper.rb
+++ lib/cms/helpers/video_helper.rb
@@ -2,23 +2,35 @@ module Cms
   module Helpers
     module VideoHelper
       def self.video_url(resource)
-        if (resource.try(:youtube_video_id).blank?) && (resource.try(:vimeo_video_id).blank?)
+        video_provider = self.video_provider(resource)
+        if !video_provider
           return nil
-        elsif resource.try(:youtube_video_id).present?
+        elsif video_provider == :youtube
           return "https://www.youtube.com/watch?v=#{resource.youtube_video_id}"
-        elsif resource.try(:vimeo_video_id).present?
+        elsif video_provider == :vimeo
           return "https://vimeo.com/#{resource.vimeo_video_id}"
         end
       end
 
 
       def self.frame_video_url(resource)
-        if (resource.try(:youtube_video_id).blank?) && (resource.try(:vimeo_video_id).blank?)
+        video_provider = self.video_provider(resource)
+        if !video_provider
           return nil
-        elsif resource.try(:youtube_video_id).present?
+        elsif video_provider == :youtube
           return "https://www.youtube.com/embed/#{resource.youtube_video_id}?controls=2&amp;showinfo=0&amp;autoplay=1&amp;autohide=1"
-        elsif resource.try(:vimeo_video_id).present?
+        elsif video_provider == :vimeo
           return "https://player.vimeo.com/video/#{resource.vimeo_video_id}?autoplay=1"
+        end
+      end
+
+      def self.video_provider(resource)
+        if resource.try(:youtube_video_id).present?
+          :youtube
+        elsif resource.try(:vimeo_video_id).present?
+          :vimeo
+        else
+          nil
         end
       end
     end
