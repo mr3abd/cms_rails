57963d4bc4049933623ecba77f8ac7eb91712452
pkorenev <p.korenev@voroninstudio.eu>
2018-02-04 14:26:13 +0200

    rails_admin image_field

--- lib/rails_admin_extensions/custom_fields.rb
+++ lib/rails_admin_extensions/custom_fields.rb
@@ -149,3 +149,27 @@ def translated_field(name, link = nil)
     end
   end
 end
+
+def image_field(name = :image, &block)
+  field name do
+    pretty_value do
+      if value.presence
+        v = bindings[:view]
+        url = resource_url
+        if image
+          thumb_url = resource_url(thumb_method)
+          image_html = v.image_tag(thumb_url, class: 'img-thumbnail', style: "max-width: 100px")
+          url != thumb_url ? v.link_to(image_html, url, target: '_blank') : image_html
+        else
+          v.link_to(nil, url, target: '_blank')
+        end
+      end
+    end
+
+
+    if block
+      self.instance_eval(&block)
+      #block.call
+    end
+  end
+end
