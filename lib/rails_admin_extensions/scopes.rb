6de7660ff5e86c5b4abf1166ea30f211b3f00450
pkorenev <p.korenev@voroninstudio.eu>
2018-05-19 20:50:09 +0300

    page_alias_model

--- lib/rails_admin_extensions/scopes.rb
+++ lib/rails_admin_extensions/scopes.rb
@@ -22,14 +22,7 @@ def __cms_rails_admin_index_controller
             scope = available_scopes[params[:scope].to_sym]
             if scope.is_a?(Proc)
               scope_proc = scope
-              method_args_count = scope_proc.arity
-              if method_args_count < 0
-                method_args_count = method_args_count * -1
-              end
-
-              available_method_args = [@objects, _current_user]
-              method_args = available_method_args[0, method_args_count]
-              @objects = scope_proc.call(*method_args)
+              @objects = call_proc_with_dynamic_arguments(scope_proc, @objects, _current_user)
             elsif scope == true
               @objects = @objects.send(params[:scope])
             end
@@ -87,4 +80,14 @@ def __cms_rails_admin_index_controller
       end
     end
   end
+end
+
+def call_proc_with_dynamic_arguments(proc, *args)
+  method_args_count = proc.arity
+  if method_args_count < 0
+    method_args_count = method_args_count * -1
+  end
+
+  method_args = args[0, method_args_count]
+  proc.call(*method_args)
 end
