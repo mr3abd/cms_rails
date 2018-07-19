6eee76d153d89a71516e4a490f34162ef8cf41ee
pasha <korenev.pasha@gmail.com>
2018-07-19 22:43:22 +0300

    app

--- app/controllers/cms/base_controller.rb
+++ app/controllers/cms/base_controller.rb
@@ -2,21 +2,6 @@ require 'cms/helpers'
 
 module Cms
   class BaseController < ::ApplicationController
-    safe_include self, ActionView::Helpers::OutputSafetyHelper
-    safe_include self, Cms::Helpers::ImageHelper
-    safe_include self, ActionView::Helpers::AssetUrlHelper
-    safe_include self, ActionView::Helpers::TagHelper
-    safe_include self, ActionView::Helpers::UrlHelper
-    safe_include self, Cms::Helpers::UrlHelper
-    safe_include self, Cms::Helpers::PagesHelper
-    safe_include self, Cms::Helpers::MetaDataHelper
-    safe_include self, Cms::Helpers::NavigationHelper
-    safe_include self, Cms::Helpers::ActionView::UrlHelper
-    safe_include self, Cms::Helpers::Breadcrumbs
-    safe_include self, ActionControllerExtensions::InstanceMethods
-    safe_include self, ::ApplicationHelper
-    safe_include self, Cms::Helpers::AnotherFormsHelper
-    safe_include self, Cms::Helpers::TagsHelper
-    safe_include self, Cms::Helpers::AssetHelper
+
   end
 end
