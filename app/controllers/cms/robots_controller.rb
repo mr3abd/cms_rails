1baeb5cdcc47ec8d9bc258ae8f9379748dd8fa09
pkorenev <p.korenev@voroninstudio.eu>
2018-10-05 13:43:56 +0300

    hide site from web.arcive.org bot in robots.txt

--- app/controllers/cms/robots_controller.rb
+++ app/controllers/cms/robots_controller.rb
@@ -10,9 +10,17 @@ module Cms
     end
 
     def lines
-      arr = ["User-agent: *"]
+      arr = []
       robots_txt_mode = ENV["ROBOTS_TXT_MODE"]
       robots_txt_production = robots_txt_mode == "production" || (Rails.env.production? && robots_txt_mode.blank?)
+
+      if robots_txt_production && ENV["ROBOTS_TXT_DISABLE_WEB_ARCHIVE"].to_s != 'false'
+        arr << 'User-agent: ia_archiver'
+        arr << 'Disallow: /'
+        arr << ''
+      end
+
+      arr << "User-agent: *"
       if robots_txt_production
         arr << "Disallow: "
       else
