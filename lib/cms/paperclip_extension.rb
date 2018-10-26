d9ec83a982e198bff549798c898c85509cc10894
pkorenev <p.korenev@voroninstudio.eu>
2018-10-26 12:50:06 +0300

    pattern

--- lib/cms/paperclip_extension.rb
+++ lib/cms/paperclip_extension.rb
@@ -5,22 +5,33 @@ Paperclip::Attachment.default_options[:old_path] = "#{Rails.root}/public#{Paperc
 
 module PaperclipExtension
   def move_images(old_path_pattern = Paperclip::Attachment.default_options[:old_path], new_path_pattern = Paperclip::Attachment.default_options[:path])
-    if self.try(:attachment_definitions).present?
+    model = self
+
+    if model.try(:attachment_definitions).present?
       puts "model: #{self.name}"
-      self.all.each do |instance|
+      model.all.each do |instance|
         puts "..id: #{instance.id}"
-        self.attachment_definitions.each do |attachment_name, attachment_definition|
+        model.attachment_definitions.each do |attachment_name, attachment_definition|
           puts "....attachment_name: #{attachment_name}"
           attachment = instance.send(attachment_name)
           style_names = (attachment.styles.keys.map(&:to_s) + ["original"]).uniq
           style_names.each do |style_name|
             puts "......style_name: #{style_name}"
-            old_path = attachment.send(:interpolate, old_path_pattern, style_name) rescue next
+            if old_path_pattern.is_a?(Proc)
+              old_path = __cms__call_proc_with_dynamic_arguments(old_path_pattern, style_name, attachment, instance, model)
+            else
+              old_path = attachment.send(:interpolate, old_path_pattern, style_name) rescue next
+            end
+
             puts "........old_path: #{old_path}; exists: #{File.exists?(old_path).inspect}"
 
             if File.exists?(old_path)
+              if new_path_pattern.is_a?(Proc)
+                new_path = __cms__call_proc_with_dynamic_arguments(new_path_pattern, style_name, attachment, instance, model)
+              else
+                new_path = attachment.send(:interpolate, new_path_pattern, style_name)
+              end
 
-              new_path = attachment.send(:interpolate, new_path_pattern, style_name)
               puts "........new_path: #{new_path}"
               new_dirname = File.dirname(new_path)
               unless File.directory?(new_dirname)
