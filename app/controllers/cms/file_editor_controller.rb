04326be0d5dc0dea670687eb19dda534d55e6d60
pkorenev <p.korenev@voroninstudio.eu>
2018-10-30 18:49:36 +0200

    app

--- app/controllers/cms/file_editor_controller.rb
+++ app/controllers/cms/file_editor_controller.rb
@@ -289,14 +289,19 @@ module Cms
     end
 
     def sanitize_filename(filename)
-      returning filename.strip do |name|
-        # NOTE: File.basename doesn't work right with Windows paths on Unix
-        # get only the filename, not the whole path
-        name.gsub!(/^.*(\\|\/)/, '')
+      # Split the name when finding a period which is preceded by some
+      # character, and is followed by some character other than a period,
+      # if there is no following period that is followed by something
+      # other than a period (yeah, confusing, I know)
+      fn = filename.split /(?<=.)\.(?=[^.])(?!.*\.[^.])/m
 
-        # Strip out the non-ascii character
-        name.gsub!(/[^0-9A-Za-z.\-]/, '_')
-      end
+      # We now have one or two parts (depending on whether we could find
+      # a suitable period). For each of these parts, replace any unwanted
+      # sequence of characters with an underscore
+      fn.map! { |s| s.gsub /[^a-z0-9\-]+/i, '_' }
+
+      # Finally, join the parts with a period and return the result
+      return fn.join '.'
     end
   end
 end
