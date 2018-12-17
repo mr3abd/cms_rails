f12eebe207a793e09a9d642951026d10bf87964a
pkorenev <p.korenev@voroninstudio.eu>
2018-12-18 00:25:30 +0200

    Cms::DbChanges.latest_changes: add select_column_names argument

--- lib/cms/db_changes.rb
+++ lib/cms/db_changes.rb
@@ -1,10 +1,13 @@
 module Cms
   module DbChanges
-    def self.latest_changes(limit = 1, min_date_time = nil, max_date_time = nil)
-      column_name = "updated_at"
+    def self.latest_changes(limit = 1, min_date_time = nil, max_date_time = nil, select_column_names = nil, date_column_name = nil)
+      select_column_names ||= [:updated_at]
+      date_column_name ||= :updated_at
+      date_column_name = date_column_name.to_s
+
       tables_to_scan = Cms.tables.select { |t| Cms.column_names(t, [column_name]).count > 0 }
       changed_tables = tables_to_scan.map do |t|
-        table_changes(t, column_name, limit, min_date_time, max_date_time)
+        table_changes(t, date_column_name, select_column_names, limit, min_date_time, max_date_time)
       end.select { |t| t[:rows].any? }.sort_by { |t| t[:rows].first[column_name] }
 
 
@@ -33,8 +36,16 @@ module Cms
       nil
     end
 
-    def self.table_changes(table_name, column_name, limit = 0, min_date_time = nil, max_date_time = nil)
-      q = "select #{column_name} from #{table_name} ORDER BY #{column_name} desc"
+    def self.table_changes(table_name, date_column_name = :updated_at, select_column_names = nil, limit = 0, min_date_time = nil, max_date_time = nil)
+      date_column_name = date_column_name.to_s
+
+      if select_column_names == :all
+        sql_column_names_str = '*'
+      else
+        sql_column_names_str = select_column_names.map(&:to_s).join(',')
+      end
+
+      q = "select #{sql_column_names_str} from #{table_name} ORDER BY #{date_column_name} desc"
       if limit.is_a?(String)
         limit = limit.to_i
       end
