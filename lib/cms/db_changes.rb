01436ee7e442d5fc23b15bd48c5d9b867f47da6d
pkorenev <p.korenev@voroninstudio.eu>
2018-12-18 00:06:23 +0200

    Cms::DbChanges.latest_changes: refactor

--- lib/cms/db_changes.rb
+++ lib/cms/db_changes.rb
@@ -1,42 +1,16 @@
 module Cms
   module DbChanges
     def self.latest_changes(limit = 1, min_date_time = nil, max_date_time = nil)
-
       column_name = "updated_at"
-      tables_to_scan = Cms.tables.select{|t| Cms.column_names(t, [column_name]).count > 0 }
-      changed_tables = tables_to_scan.map{|t|
-        q = "select #{column_name} from #{t} ORDER BY #{column_name} desc"
-        if limit.is_a?(String)
-          limit = limit.to_i
-        end
-
-        if limit.is_a?(Numeric) && limit > 0
-          q = q + " LIMIT " + limit.to_s
-        end
-
-        rows = ActiveRecord::Base.connection.execute(q)
-        rows = rows.map{|r| Hash[column_name, DateTime.parse(r[column_name])] }
-        if min_date_time
-          if min_date_time.is_a?(String)
-            min_date_time = DateTime.parse(min_date_time)
-          end
-          rows = rows.select{|r| r[column_name] >= min_date_time  }
-        end
-
-        if max_date_time
-          if max_date_time.is_a?(String)
-            max_date_time = DateTime.parse(max_date_time)
-          end
-          rows = rows.select{|r| r[column_name] <= max_date_time  }
-        end
-
-        {table: t, rows: rows}
-      }.select{|t| t[:rows].any? }.sort_by{|t| t[:rows].first[column_name] }
+      tables_to_scan = Cms.tables.select { |t| Cms.column_names(t, [column_name]).count > 0 }
+      changed_tables = tables_to_scan.map do |t|
+        table_changes(t, column_name, limit, min_date_time, max_date_time)
+      end.select { |t| t[:rows].any? }.sort_by { |t| t[:rows].first[column_name] }
 
 
-      table_top_separator = 10.times.map{"="}.join("")
-      table_header_separator = 10.times.map{"-"}.join("")
-      table_bottom_separator = 10.times.map{"="}.join("") + "\n\n\n"
+      table_top_separator = 10.times.map { "=" }.join("")
+      table_header_separator = 10.times.map { "-" }.join("")
+      table_bottom_separator = 10.times.map { "=" }.join("") + "\n\n\n"
 
       no_changes = changed_tables.blank?
       if no_changes
@@ -45,7 +19,6 @@ module Cms
         puts table_top_separator
         return nil
       end
-
 
       changed_tables.each do |t|
         puts table_top_separator
@@ -58,6 +31,36 @@ module Cms
       end
 
       nil
+    end
+
+    def self.table_changes(table_name, column_name, limit = 0, min_date_time = nil, max_date_time = nil)
+      q = "select #{column_name} from #{table_name} ORDER BY #{column_name} desc"
+      if limit.is_a?(String)
+        limit = limit.to_i
+      end
+
+      if limit.is_a?(Numeric) && limit > 0
+        q = q + " LIMIT " + limit.to_s
+      end
+
+      rows = ActiveRecord::Base.connection.execute(q)
+      rows = rows.map { |r| Hash[column_name, DateTime.parse(r[column_name])] }
+
+      if min_date_time
+        if min_date_time.is_a?(String)
+          min_date_time = DateTime.parse(min_date_time)
+        end
+        rows = rows.select { |r| r[column_name] >= min_date_time  }
+      end
+
+      if max_date_time
+        if max_date_time.is_a?(String)
+          max_date_time = DateTime.parse(max_date_time)
+        end
+        rows = rows.select{|r| r[column_name] <= max_date_time  }
+      end
+
+      {table: table_name, rows: rows}
     end
   end
 end
