41a97e6f2e1f8865ebed809176f47f40e3964e28
pkorenev <p.korenev@voroninstudio.eu>
2018-12-18 01:36:54 +0200

    Cms::DbChanges.latest_changes: write to file or console

--- lib/cms/db_changes.rb
+++ lib/cms/db_changes.rb
@@ -1,6 +1,7 @@
 module Cms
   module DbChanges
-    def self.latest_changes(limit = 1, min_date_time = nil, max_date_time = nil, select_column_names = nil, date_column_name = nil)
+    def self.latest_changes(limit = 1, min_date_time = nil, max_date_time = nil, select_column_names = nil, date_column_name = nil, output_target = nil)
+      output_target ||= :console
       select_column_names ||= [:updated_at]
       date_column_name ||= :updated_at
       date_column_name = date_column_name.to_s
@@ -10,28 +11,42 @@ module Cms
         table_changes(t, date_column_name, select_column_names, limit, min_date_time, max_date_time)
       end.select { |t| t[:rows].any? }.sort_by { |t| t[:rows].first[date_column_name] }
 
-
       table_top_separator = 10.times.map { "=" }.join("")
       table_header_separator = 10.times.map { "-" }.join("")
       table_bottom_separator = 10.times.map { "=" }.join("") + "\n\n\n"
 
+      output = ""
+      options = {
+        limit: limit,
+        min_date_time: min_date_time,
+        max_date_time: max_date_time,
+        select_column_names: select_column_names,
+        date_column_name: date_column_name
+      }
+
       no_changes = changed_tables.blank?
       if no_changes
-        puts table_top_separator
-        puts "NO CHANGES"
-        puts table_top_separator
+        output += "\n" + table_top_separator
+        output += "\n" + "NO CHANGES"
+        output += "\n" + table_top_separator
+        output_target_info = write_db_changes(options, output, output_target)
+        if output_target_info.is_a?(Hash) && output_target_info[:message].present?
+          puts output_target_info[:message]
+        end
         return nil
       end
 
       changed_tables.each do |t|
-        puts table_top_separator
-        puts t[:table]
-        puts table_header_separator
+        table_top_separator
+        output += "\n" + t[:table]
+        output += "\n" + table_header_separator
         t[:rows].each do |r|
-          puts r.inspect
+          output += "\n" + r.inspect
         end
-        puts table_bottom_separator
+        output += "\n" + table_bottom_separator
       end
+
+      write_db_changes_to_file(options, output)
 
       nil
     end
@@ -90,6 +105,40 @@ module Cms
 
         r
       }
+    end
+
+    def self.write_db_changes(options, content, output_target)
+      if output_target.to_s == 'file'
+        file_path = write_db_changes_to_file(options, content)
+        return { message: "File saved at #{file_path}" }
+      else
+        puts content
+      end
+    end
+
+    def self.write_db_changes_to_file(options, content)
+      file_dirname = Rails.root.join('db/').to_s
+      file_name = "db_changes"
+      if options[:min_date_time]
+        file_name += "_from_#{options[:min_date_time].to_s.underscore}"
+      end
+
+      if options[:max_date_time]
+        file_name += "_to_#{options[:max_date_time].to_s.underscore}"
+      end
+
+      file_name += "_at_#{DateTime.now.to_s.underscore}"
+      file_name += '.txt'
+
+      file_path = "#{file_dirname}#{file_name}"
+
+      header = "DbChanges.latest_changes options: " + options.inspect
+
+      full_content = "#{header}\n\n#{content}"
+
+      File.write(file_path, full_content)
+
+      return file_path
     end
   end
 end
